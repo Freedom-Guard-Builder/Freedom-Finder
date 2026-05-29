e2e772c17e7471f9c8307ba87f83104c53681124
FG Builder <FreedomGuard@duck.com>
2026-05-29 14:00:47 +0330

    add

---
+++ app/sources/github/rayan_config.py
@@ -0,0 +1,2 @@
+CHANNEL_NAME = "Rayan_Config"
+CHANNEL_URL = "https://raw.githubusercontent.com/Rayan-Config/C-Sub/refs/heads/main/configs/proxy.txt"
--- app/sources/telegram/rayan_config.py
+++
@@ -1,2 +0,0 @@
-CHANNEL_NAME = "Rayan_Config"
-CHANNEL_URL = "https://t.me/s/Rayan_Config"
